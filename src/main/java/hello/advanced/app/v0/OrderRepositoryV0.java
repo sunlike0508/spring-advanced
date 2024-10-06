1f5e3b9f3f66fc9ff76b4d6287850df3c190b17e
seonhoshin <sunlike0508@gmail.com>
2024-10-06 21:22:38 +0900

    기본 생성

---
+++ src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,29 @@
+package hello.advanced.app.v0;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId) {
+
+        if(itemId.equals("ex")) {
+            throw new IllegalStateException("예외 발생");
+        }
+
+        sleep(1000);
+    }
+
+
+    private void sleep(int mSecond) {
+
+        try {
+            Thread.sleep(mSecond);
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}

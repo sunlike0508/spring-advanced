d60236a764b77c1e6084ce74617fdfca8ca7684d
seonhoshin <sunlike0508@gmail.com>
2024-10-09 16:41:57 +0900

    전략패턴

--- src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -2,8 +2,39 @@ package hello.advanced.trace.strategy;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @Slf4j
 @SpringBootTest
-public class ContextV1Test {}
+class ContextV1Test {
+
+    @Test
+    void templateMethod() {
+        logic1();
+        logic2();
+    }
+
+
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis();
+
+        log.info("비즈니스 로직1 실행");
+
+        long endTime = System.currentTimeMillis();
+
+        log.info("resultTime = {} ", endTime - startTime);
+    }
+
+
+    private void logic2() {
+        long startTime = System.currentTimeMillis();
+
+        log.info("비즈니스 로직2 실행");
+
+        long endTime = System.currentTimeMillis();
+
+        log.info("resultTime = {} ", endTime - startTime);
+    }
+}
